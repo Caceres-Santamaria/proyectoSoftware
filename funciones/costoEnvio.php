0e0dbc8f49ddd4da0d664c2e5f5b217cb3304668
jmsr06 <jennysantamaria06@gmail.com>
2020-09-16 17:37:54 -0500

    arrglo detalle pedido

--- funciones/costoEnvio.php
+++ funciones/costoEnvio.php
@@ -2,64 +2,65 @@
 include '../funciones/Metodos.php';
 $objMetodo = new Metodos();
 $id = $_REQUEST['id'];
-
-if($_POST['codigo'] != 0)
+if($id == 0 || $id==1 || $id==2)
 {
-    $costo = $objMetodo->costoEnvio($_POST['codigo']);
-    if($id == 0){
-        echo "$ ".number_format($costo,1);
-    }
-    elseif($id == 1)
+    if($_POST['codigo'] != 0)
     {
-        $subtotal = $_POST['subtotal'];
-        $cupon = $_POST['cupon'];
-        if($cupon == "")
-        {
-            $total = $subtotal + $costo;
+        $costo = $objMetodo->costoEnvio($_POST['codigo']);
+        if($id == 0){
+            echo "$ ".number_format($costo,1);
         }
-        else
+        elseif($id == 1)
         {
-            $valor = $objMetodo->getDescuento($cupon);
-            $descuento = $subtotal*$valor;
-            $total = $subtotal + $costo - $descuento;
-        }
-        echo "$ ".number_format($total,1);
-    }
-    elseif($id == 2)
-    {
-        $subtotal = $_POST['subtotal'];
-        $cupon = $_POST['cupon'];
-        if($cupon == "")
-        {
-            $total = $subtotal + $costo;
-        }
-        else
-        {
-            $cont = $objMetodo->contCupon($cupon);
-            if($cont==1)
+            $subtotal = $_POST['subtotal'];
+            $cupon = $_POST['cupon'];
+            if($cupon == "")
+            {
+                $total = $subtotal + $costo;
+            }
+            else
             {
                 $valor = $objMetodo->getDescuento($cupon);
                 $descuento = $subtotal*$valor;
                 $total = $subtotal + $costo - $descuento;
             }
-            else
+            echo "$ ".number_format($total,1);
+        }
+        elseif($id == 2)
+        {
+            $subtotal = $_POST['subtotal'];
+            $cupon = $_POST['cupon'];
+            if($cupon == "")
             {
                 $total = $subtotal + $costo;
             }
+            else
+            {
+                $cont = $objMetodo->contCupon($cupon);
+                if($cont==1)
+                {
+                    $valor = $objMetodo->getDescuento($cupon);
+                    $descuento = $subtotal*$valor;
+                    $total = $subtotal + $costo - $descuento;
+                }
+                else
+                {
+                    $total = $subtotal + $costo;
+                }
+            }
+            echo "$ ".number_format($total,1);
         }
-        echo "$ ".number_format($total,1);
     }
-}
-elseif($id == 0)
-{
-    $costo = 0;
-    echo "$ ".number_format($costo,1);
-}
-elseif($id==1)
-{
-    $subtotal = $_POST['subtotal'];
-    //echo "$ ".number_format($subtotal,1);
-    $cupon = $_POST['cupon'];
+    elseif($id == 0)
+    {
+        $costo = 0;
+        echo "$ ".number_format($costo,1);
+    }
+    elseif($id==1)
+    {
+        $subtotal = $_POST['subtotal'];
+        //echo "$ ".number_format($subtotal,1);
+        $cupon = $_POST['cupon'];
         if($cupon == "")
         {
             $total = $subtotal;
@@ -71,30 +72,31 @@ elseif($id==1)
             $total = $subtotal  - $descuento;
         }
         echo "$ ".number_format($total,1);
-}
-elseif($id == 2)
-{
-    $subtotal = $_POST['subtotal'];
-    $cupon = $_POST['cupon'];
-    if($cupon == "")
-    {
-        $total = $subtotal;
     }
-    else
+    elseif($id == 2)
     {
-        $cont = $objMetodo->contCupon($cupon);
-        if($cont==1)
-        {
-            $valor = $objMetodo->getDescuento($cupon);
-            $descuento = $subtotal*$valor;
-            $total = $subtotal - $descuento;
-        }
-        else
+        $subtotal = $_POST['subtotal'];
+        $cupon = $_POST['cupon'];
+        if($cupon == "")
         {
             $total = $subtotal;
         }
+        else
+        {
+            $cont = $objMetodo->contCupon($cupon);
+            if($cont==1)
+            {
+                $valor = $objMetodo->getDescuento($cupon);
+                $descuento = $subtotal*$valor;
+                $total = $subtotal - $descuento;
+            }
+            else
+            {
+                $total = $subtotal;
+            }
+        }
+        echo "$ ".number_format($total,1);
     }
-    echo "$ ".number_format($total,1);
 }
 elseif($id==3)
 {
